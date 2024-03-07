4936c1e8db4088f7b4a1f5d73606b7393f26c4af
Jason Holland <jholland918@gmail.com>
2024-03-07 17:12:47 -0600

    init

---
+++ BuildDemo1/Assets/RotateObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public Vector3 RotateAmount;  // degrees per second to rotate in each axis. Set in inspector.
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateAmount * Time.deltaTime);
+    }
+}
